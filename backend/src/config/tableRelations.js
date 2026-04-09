22f16012e06982d4f9245260ffede64ecd22da5e
NehaArshad <nehaarshad1203@gmail.com>
2026-04-09 12:05:45 +0500

    manage user access controll and get them along with their complete associations

--- backend/src/config/tableRelations.js
+++ backend/src/config/tableRelations.js
@@ -89,6 +89,8 @@ export default function relations() {
   Student.hasMany(StudentGuardian, { foreignKey: "studentId" });
   StudentGuardian.belongsTo(Student, { foreignKey: "studentId" });
 
+Student.belongsTo(BatchModel, { foreignKey: "batchId" });
+BatchModel.hasMany(Student, { foreignKey: "batchId" });
   // ==================== Program ====================
 
   BatchModel.belongsTo(ProgramModel, { foreignKey: "programId" });
@@ -151,19 +153,6 @@ export default function relations() {
   // ==================== Course & Offerings ====================
   CourseOfferingModel.hasMany(CourseModel, { foreignKey: "courseId" });
   CourseModel.belongsTo(CourseOfferingModel, { foreignKey: "courseId" });
-
-  // CourseModel.belongsToMany(CategoryModel, {
-  //   through: CourseCategoryModel,
-  //   foreignKey: "courseId",
-  //   otherKey: "categoryId",
-  //   as: "categories"
-  // });
-  // CategoryModel.belongsToMany(CourseModel, {
-  //   through: CourseCategoryModel,
-  //   foreignKey: "categoryId",
-  //   otherKey: "courseId",
-  //   as: "courses"
-  // });
 
   CourseCategoryModel.belongsTo(CourseModel, { foreignKey: "courseId" });
   CourseCategoryModel.belongsTo(CategoryModel, { foreignKey: "categoryId" });
