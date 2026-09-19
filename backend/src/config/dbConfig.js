5e7a3eec74ee709aff7eead33810da66da280c69
Valeeja <valeeja.jamil19@gmail.com>
2026-09-19 14:52:12 +0500

    resolve env variable mapping and connection config

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,18 +1,20 @@
-import { Sequelize } from "sequelize"
-import dotenv from "dotenv"
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-const sequelize= new Sequelize(
-    process.env.DATABASE,
-    process.env.USERNAME,
-    process.env.PASSWORD,
-    {
-       host: process.env.HOST ,
-       dialect:"mysql",
-      port: process.env.DB_PORT || 3307,
-       logging: false,  
-    }
-)
+const dbName = process.env.DB_NAME || process.env.DATABASE || "adviso";
+// process.env.DB_USER pehle check hoga taake OS USERNAME ('valee') bypass ho sakay
+const dbUser = process.env.DB_USER || "root"; 
+const dbPassword = process.env.DB_PASSWORD || process.env.PASSWORD || "";
+const dbHost = process.env.DB_HOST || process.env.HOST || "127.0.0.1";
+const dbPort = Number(process.env.DB_PORT) || 3306;
 
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: "mysql",
+  port: dbPort,
+  logging: false,
+});
 
 export default sequelize;
